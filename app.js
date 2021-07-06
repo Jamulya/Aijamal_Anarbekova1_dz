f838e6a8c314bc5f562f1d87733016dcd325a4d5
Jamulya <65002206+Jamulya@users.noreply.github.com>
2021-07-06 00:26:05 -0600

    dz1
    dz1

---
+++ app.js
@@ -0,0 +1,40 @@
+// 1 
+// alert("Hello");
+// var userName = prompt("What is your name?", "userName");
+//     console.log(userName);
+// var lastName = prompt("What is your last name?", "lastName");
+//     console.log(lastName);
+//     alert(`Hello, ${userName} ${lastName}!`)
+
+
+//2
+// var num1 = prompt('Введите число : ');
+// var num2 = parseInt(prompt('Введите второе число : '));
+//     if (num1 > num2) {
+//         alert(num1 + ' больше');
+//     }else{
+//     alert(num2 + 'больше');
+//     }
+//     if(num1 == num2) {
+//         alert('числа равны');
+//     }
+
+
+//3
+// var color = "red"
+// switch (color) {
+//     case "red":
+//         console.log("Stop");
+//         break;
+//     case "yellow":
+//         console.log("wait");
+//         break;
+//     case "green":
+//         console.log("go");
+//         break;
+
+//     default:
+//         console.log("Unknown color");
+// }
+
+
